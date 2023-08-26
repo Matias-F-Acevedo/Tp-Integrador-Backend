bdc965d5b3090110add4356d4b0104357a5b1770
Matias F. Acevedo <matias.acevedo56732@gmail.com>
2023-08-26 19:46:16 -0300

    metodos Delete y Put + codigos de respuesta

--- src/propiedad/propiedad.controller.ts
+++ src/propiedad/propiedad.controller.ts
@@ -1,33 +1,75 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
-import { PropiedadService} from './propiedad.service';
+import { Body, Controller, Delete, Get, Param, Post, Put, Res, HttpStatus, NotFoundException, BadRequestException } from '@nestjs/common';
+import { PropiedadService } from './propiedad.service';
 import { Propiedad } from './propiedad.interface';
-
+import { Response } from 'express';
 @Controller('propiedad')
 export class PropiedadController {
 
-    constructor(private readonly PropiedadService: PropiedadService){}
+  constructor(private readonly PropiedadService: PropiedadService) { }
 
-    @Get()
-    getPropiedades(): Promise <Propiedad[]>{
-        return this.PropiedadService.getPropiedades();
+  @Get()
+  async getPropiedades(@Res() res: Response) {
+    try {
+      const serviceResponse = await this.PropiedadService.getPropiedades();
+      return res.status(HttpStatus.OK).send(serviceResponse)
+    } catch (error) {
+      throw new NotFoundException("Not found")
     }
-    @Get("/:id")
-    getPropiedadById(@Param("id") id:string): Promise <Propiedad>{
-        return this.PropiedadService.getPropiedadById(id);
+  }
+
+
+
+  @Get("/:id")
+  async getPropiedadById(@Res() res: Response, @Param("id") id: string) {
+    try {
+      const serviceResponse = await this.PropiedadService.getPropiedadById(id);
+      return res.status(HttpStatus.OK).send(serviceResponse)
+    } catch (error) {
+      throw new NotFoundException("Not found")
+    }
+  }
+
+
+  @Post()
+  async postPropiedad(@Res() res: Response, @Body() propiedad: any) {
+    try {
+
+      const serviceResponse = await this.PropiedadService.postPropiedad(propiedad)
+      return res.status(HttpStatus.CREATED).send({ message: "Created", data: serviceResponse, success: true, code: HttpStatus.CREATED })
+
+    } catch (error) {
+      throw new BadRequestException("Propiedad creation failed")
     }
 
-    @Post()
-    postPropiedad(@Body() propiedad: any){
-    const nuevaPropiedad = this.PropiedadService.postPropiedad(propiedad)
-    return {
-    message: 'Data saved',
-    propiedad: propiedad,
-    success: true,
-    code: 201,
-    data: nuevaPropiedad
   }
+
+
+
+  @Delete(":id")
+  async deletepropiedadById(@Res() res: Response, @Param('id') id: string) {
+    try {
+      const serviceResponse = await this.PropiedadService.deletePropiedadById(id);
+      return res.status(HttpStatus.OK).send({ message: serviceResponse.message, success: serviceResponse.success, code: HttpStatus.OK })
+    } catch (error) {
+      throw new NotFoundException("Delete failed")
+    }
   }
-    
+
+
+
+  @Put(':id')
+  async updateTrackById(@Res() res: Response, @Param('id') id: string, @Body() body: any) {
+    try {
+      const serviceResponse = await this.PropiedadService.updateTrackById(id, body);
+      return res.status(HttpStatus.OK).send({ message: serviceResponse.message, success: serviceResponse.success, code: HttpStatus.OK, data: serviceResponse.data})
+    } catch (error) {
+      throw new NotFoundException("Update failed")
+    }
+
+  }
+
+
+
 }
 
 
--- src/propiedad/propiedad.service.ts
+++ src/propiedad/propiedad.service.ts
@@ -1,39 +1,86 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Propiedad } from './propiedad.interface';
 // import{createID,readParse } from '../utils/utils'
 import * as fs from 'fs'
 import { uuid } from 'uuidv4';
-import {join} from 'path'
+import { join } from 'path'
+import { error } from 'console';
 
 const URL = "http://localhost:3030/propiedad"
 
 @Injectable()
 export class PropiedadService {
 
-    async getPropiedades(): Promise <Propiedad[]>{
-        const res = await fetch(URL);
-        const parsed = await res.json();
-        return parsed;
-    }
-    async getPropiedadById(id:string): Promise <Propiedad>{
-        const res = await fetch(`${URL}/${id}`);
-        const parsed = await res.json();
-        return parsed;
-    }
-
-
-    async postPropiedad(propiedad: any){
+    async getPropiedades() {
         try {
-            const newPropiedad = {id:uuid(), ... propiedad }
-           
-            const data = JSON.parse(fs.readFileSync((join(__dirname, '../../data/data.json')), 'utf-8'));
-          
-            data.propiedad.push(newPropiedad);
-            fs.writeFileSync((join(__dirname, '../../data/data.json')), JSON.stringify(data, null, 2))
-            return data
+            const res = await fetch(URL);
+            const parsed = await res.json();
+            return parsed;
+        } catch {
+            throw new error()
+        }
+
+    }
+
+    async getPropiedadById(id: string) {
+        const res = await fetch(`${URL}/${id}`);
+        if (!res.ok) {
+            throw new NotFoundException("Data not found");
+        } else {
+            const parsed = await res.json();
+            return parsed;
+        }
+    }
+
+
+
+
+
+    async postPropiedad(propiedad: any) {
+        try {
+            const newPropiedad = { id: uuid(), ...propiedad }
+            await fetch(URL, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(newPropiedad),
+            });
+            return propiedad
         } catch (error) {
             throw new Error("Created failed");
         }
     }
-    
+
+
+    async deletePropiedadById(id: string) {
+            const res = await fetch(`${URL}/${id}`, {
+                method: 'DELETE',
+            });
+
+            if (res.ok) {
+                return { success: true, message: `Propiedad with id: {${id}} was deleted` };
+            } else {
+                throw new error()
+            }
+    }
+
+
+    async updateTrackById(id: string, body: any) {
+        const isPropiedad = await this.getPropiedadById(id);
+        if (isPropiedad) {
+            const updatedPropiedad = { id, ...body };
+            await fetch(`${URL}/${id}`, {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(updatedPropiedad),
+            });
+            return { success: true, message: `Propiedad edited`, data: `${body}` };
+        } else {
+            throw new error()
+        }
+    }
 }
+
