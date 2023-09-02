9423ed844f11ce3cec50d8d8cc13b1a3893fb591
Matias F. Acevedo <matias.acevedo56732@gmail.com>
2023-09-02 14:35:56 -0300

    validaciones y DTO para las entidades propiedad y usuario

--- backend-NestJs/src/usuario/usuario.service.ts
+++ backend-NestJs/src/usuario/usuario.service.ts
@@ -1,9 +1,11 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
-import { Usuario } from './usuario.interface';
 import * as fs from 'fs'
-import { uuid } from 'uuidv4';
+import { v4 as uuidv4 } from 'uuid';
 import { join } from 'path'
 import { error } from 'console';
+import { PropiedadDto } from 'src/propiedad/propiedad.dto';
+import { Usuario_id_Dto } from './usuario_id.dto';
+import { UsuarioDto } from './usuario.dto';
 const URL = "http://localhost:3030/usuario"
 
 
@@ -11,13 +13,13 @@ const URL = "http://localhost:3030/usuario"
 @Injectable()
 export class UsuarioService {
 
-    async getUsuarios() {
+    async getUsuarios():Promise<Usuario_id_Dto[]> {
 
         try {
             const res = await fetch(URL);
             const parsed = await res.json();
-            const arrayPropiedades: any[] = []
-            parsed.map((user: { id: any; nombre: any; apellido: any; email: any; propiedades: any;}) => {
+            const arrayPropiedades: Usuario_id_Dto[] = []
+            parsed.map((user: { id: string; nombre: string; apellido: string; email: string; propiedades: number[]}) => {
                 arrayPropiedades.push({
                     id: user.id,
                     nombre: user.nombre,
@@ -34,27 +36,27 @@ export class UsuarioService {
 
     }
 
-    async getUsuarioById(id: string) {
+    async getUsuarioById(id: string):Promise<Usuario_id_Dto> {
         const res = await fetch(`${URL}/${id}`);
         if (!res.ok) {
             throw new NotFoundException("Data not found");
         } else {
             const parsed = await res.json();
-            return ({
+            return {
                 id: parsed.id,
                 nombre: parsed.nombre,
                 apellido: parsed.apellido,
                 email: parsed.email,
                 propiedades: parsed.propiedades
-            });
+            };
         }
 
     }
 
 
-    async postUsuario(usuario: any) {
+    async postUsuario(usuario: UsuarioDto) {
         try {
-            const newUsuario = { id: uuid(), ...usuario }
+            const newUsuario = { ...usuario ,id: (uuidv4().slice(0, -28))}
             await fetch(URL, {
                 method: 'POST',
                 headers: {
@@ -62,10 +64,38 @@ export class UsuarioService {
                 },
                 body: JSON.stringify(newUsuario),
             });
-            return usuario
+            return {...newUsuario, contraseña: "********"};
 
         } catch (error) {
             throw new Error("Created failed");
+        }
+    }
+
+    async deleteUserById(id: string):Promise<{success:boolean, message:string}>{
+        const res = await fetch(`${URL}/${id}`, {
+            method: 'DELETE',
+        });
+        if (res.ok) {
+            return { success: true, message: `User with id: {${id}} was deleted`};
+        } else {
+            throw new error()
+        }
+    }
+
+    async updateUserById(id:string, body: any){
+        const isUser = await this.getUsuarioById(id);
+        if (isUser) {
+            const updateUser = {id: id, ...body };
+            await fetch(`${URL}/${id}`, {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(updateUser),
+            });
+            return { success: true, message: `User edited`, data: `${{...updateUser, contraseña: "********"}}` };
+        } else {
+            throw new error()
         }
     }
 
